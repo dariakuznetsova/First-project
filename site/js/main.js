4215cf82b5f90899f27bd22f151bdf97d9ff7bf4
Daria Kuznetsova <kds2108@list.ru>
2016-01-27 23:35:16 +0300

    Вторая страница готова

---
+++ site/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+$("input").focus(function() {
+	$(this).attr({"value":""});
+});
+
+
+
+    // $("#searchbar").blur(function() {
+
+    //  $(this).removeClass("active");
+
+    //     if($(this).attr("value") == "") $(this).attr("value", text);
