52a4f1ec5914145f9a452029864a842684793a4d
Daria Kuznetsova <kds2108@list.ru>
2016-01-28 12:59:31 +0300

    добавила README

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- Инструкция -->
+
+1. git clone https://github.com/dariakuznetsova/First-project
+2. cd site
+3. bower i
+4. npm i
--- site/js/main.js
+++ site/js/main.js
@@ -2,14 +2,3 @@
 
 
 
-$("input").focus(function() {
-	$(this).attr({"value":""});
-});
-
-
-
-    // $("#searchbar").blur(function() {
-
-    //  $(this).removeClass("active");
-
-    //     if($(this).attr("value") == "") $(this).attr("value", text);
