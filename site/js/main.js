7a84e326934bbfa540d86bf489bdc7849393140c
Daria Kuznetsova <kds2108@list.ru>
2016-02-06 19:24:27 +0300

    Последнее, форма не до конца

--- site/js/main.js
+++ site/js/main.js
@@ -27,7 +27,7 @@
 
 // Валидация
 
-var validationFn = (function () {
+/*var validationFn = (function () {
 
   var init = function () {
         _setUpListners();
@@ -99,6 +99,86 @@ var validationFn = (function () {
       init: init
   };
   
+  
+})();
+
+validationFn.init();*/
+
+
+
+
+
+
+var validationFn = (function () {
+
+  var init = function () {
+        _setUpListners();
+      },
+      form = $(".form");
+      
+  var _setUpListners = function ()  {
+        	$(form).on('submit', function(e) {
+					e.preventDefault();
+					checkForm(form);
+				});
+
+				$('.close').on('click', function() {
+					$(form).trigger('reset');
+					$(".inputs").css({"background-color":"white", "border-color":"#48cbe8"});
+					$('.tooltips').remove(); // удаляем все тултипы
+          
+       //По клику на крестик пропадает зеленое окно об успешном добавлении проекта
+	$('body').on('click', '.close2',function() {
+		$('.added').hide(); 	
+	});	   
+
+		});
+      
+		$(form).on("keydown", '.error', function(){ // попробуй все-таки не через фокус а через keydown
+			$(this).css({"background-color":"white", "border-color":"#48cbe8"});
+			$(this).parent('.tooltips').remove();
+		});
+    };
+
+   function checkForm (form) {
+			var form = $(form),
+			inputs = form.find('.inputs');
+
+		
+			$(inputs).each(function (index,elem){
+				if($(elem).val() == "") {
+		 			$(elem).css({"background-color":"#fad6d4", "border-color":"#f97e76"});
+		 			$(elem).addClass('error');
+		 			createTooltip(elem);
+		 		};
+			});
+
+ 			if(!$('.error').length) {
+				$('.added').show();
+				$(form).trigger('reset');
+			};
+		
+		};		
+	
+	function createTooltip (elem) {
+	
+		var position = $(elem).data('tooltip-position');
+		var text = $(elem).data('tooltip-text');
+		
+		if(position == 'left') {
+			var tooltip = ('<div class="tooltips tooltip_left">' + text + '</div>');			
+		}else if(position =='right') {
+			var tooltip = ('<div class="tooltips tooltip_right">' + text + '</div>');
+		};
+
+		$(elem).parent().append(tooltip); // добавляем тултип относительно родителя
+	};
+  
+
+  
+  return {
+      init: init
+  };
   
 })();
 
