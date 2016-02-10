6a99cf25567ac06fa25525c908fdcaf4209babe9
Daria Kuznetsova <kds2108@list.ru>
2016-02-10 11:54:52 +0300

    самый окончательный вариант (но еще до конкатенации)

--- site/js/main.js
+++ site/js/main.js
@@ -1,10 +1,13 @@
 
-// bPopup
+// модуль bPopup
+var popup = (function() {
 
-(function($) {
+	var init = function () {
+		b_popup();
+	};
 
-	$(function() {
-    	$('.a_add_pr').bind('click', function(e) {
+	var b_popup = function () {
+		$('.a_add_pr').bind('click', function(e) {
         	e.preventDefault();
  			if ($('.added')) {
          		 $('.added').hide();  
@@ -14,28 +17,49 @@
 			$(this).removeClass('error');
 
    			$('.form_wrapper').bPopup();
-   		});
-    });
+		});
+	};
 
-})(jQuery);
+	return {
+		init: init
+	};
+
+})();
+
+if($('.a_add_pr')) {
+	popup.init();
+};
 
 
-// Загрузка файла. Выведение названия файла в поле input text из input file
 
-  function dwnlFn() {
-  	var picture = document.getElementById('picture'),
-  		dwnl = document.getElementById('dwnl'),
+// Модуль загрузки файла. Выведение названия файла в поле textarea из input file
+// Не забыть прописать атрибут onchange="download_fn.init();" для input file в html
+
+var download_fn = (function () {
+
+	var init = function () {
+		dwnl_fn();
+		},
+		picture = document.getElementById('picture'),
+		dwnl = document.getElementById('dwnl'),
   		sec_in = $(".second_input");
 
-  	picture.value = dwnl.value.replace(/.+[\\\/]/, "");
+	var dwnl_fn = function () {
+		picture.value = dwnl.value.replace(/.+[\\\/]/, "");
+		$(sec_in).css({"background-color":"white", "border-color":"#48cbe8"});
+		$(sec_in).removeClass('error');
+		$(sec_in).next().remove();
+	};
 
-	$(sec_in).css({"background-color":"white", "border-color":"#48cbe8"});
-	$(sec_in).removeClass('error');
-	$(sec_in).next().remove();
- };
+	return {
+		init: init
+	};
+
+})();
 
 
-// Валидация на странице my_works
+
+// Валидация
 
 var validationFn = (function () {
 
@@ -51,15 +75,11 @@ var validationFn = (function () {
 				}else{
     				e.returnValue = false; // Условие для IE8
 				};
-
 					checkForm(form);
 				});
 
 			$('.close').on('click', function() {
 				$(form).trigger('reset');
-				// $(".inputs").css({"background-color":"white", "border-color":"#48cbe8"});
-				// $('.tooltips').remove(); 
-				// $(this).removeClass('error');
         	});  
 
 			$('body').on('click', '.close2',function() {
@@ -71,90 +91,19 @@ var validationFn = (function () {
 				$(this).next().remove();
 				$(this).removeClass('error');
 			});
-    };
 
-   function checkForm (form) {
-			var form = $(form),
-			inputs = form.find('.inputs').not('input[type="file"], input[type="hidden"]');
-			$('.tooltips').remove();
-		
-			$(inputs).each(function (index,elem){
-				if($(elem).val() == "") {
-		 			$(elem).css({"background-color":"#fad6d4", "border-color":"#f97e76"});
-		 			$(elem).addClass('error');
-		 			createTooltip(elem);
-		 		};
-			});
-
- 			if($('.error').length == 0) {
-				$('.added').show();
-				$(form).trigger('reset');
-			};
-		
-		};		
-	
-	function createTooltip (elem) {
-	
-		var position = $(elem).data('tooltip-position');
-		var text = $(elem).data('tooltip-text');
-		
-		if(position == 'left') {
-			var tooltip = ('<div class="tooltips">' + text + '</div>');			
-		};
-
-		$(elem).parent().append(tooltip); 
-		$('#description').next('.tooltips').css({'right':'102%', 'top':'32%'});
-	};
-  
-  
-  return {
-      init: init
-  };
-  
-})();
-
-validationFn.init();
-
-
-
-// Валидация на странице contact_me
-
-var validationFn2 = (function () {
-
-  var init = function () {
-        _setUpListners();
-      },
-      form = $(".contact_form");
-      
-  var _setUpListners = function ()  {
-        	$(form).on('submit', function(e) {
-					
-        		if(e.preventDefault){
-    				e.preventDefault();
-				}else{
-    				e.returnValue = false; // Условие для IE8
-				};
-					checkForm(form);
-				});
-
-			$(form).on('reset', function(e) {
-				$('.input').css({"background-color":"white", "border-color":"#48cbe8"});
+			$(form).on('reset', function() {
+				$('.inputs').css({"background-color":"white", "border-color":"#48cbe8"});
 				$('.tooltips').remove();
 				$(this).removeClass('error');
 			});
-
-			$(form).on("keydown", '.error', function(){ 
-				$(this).css({"background-color":"white", "border-color":"#48cbe8"});
-				$(this).next().remove();
-				$(this).removeClass('error');
-			});
     };
 
-   function checkForm (form) {
+   var checkForm = function (form) {
 			var form = $(form),
-			inputs = form.find('.input').not('input[type="file"], input[type="hidden"]');
+			inputs = form.find('.inputs').not('input[type="file"], input[type="hidden"]');
 			$('.tooltips').remove();
-		
+
 			$(inputs).each(function (index,elem){
 				if($(elem).val() == "") {
 		 			$(elem).css({"background-color":"#fad6d4", "border-color":"#f97e76"});
@@ -170,29 +119,34 @@ var validationFn2 = (function () {
 		
 		};		
 	
-	function createTooltip (elem) {
+	var createTooltip = function (elem) {
 	
 		var text = $(elem).data('tooltip-text'),
 			position = $(elem).data('tooltip-position');
-
+			
 			if(position == 'left') {
 				$(elem).removeClass('tooltip-right');
 				var tooltip1 = ('<div class="tooltips tooltip-left">' + text + '</div>');
 				$(elem).parent().append(tooltip1); 
 				$('#third_inp').next('.tooltip-left').css({'right':'101.5%', 'top':'42%'});
+				$('#description').next('.tooltips').css({'right':'102%', 'top':'32%'});
 			}if(position == 'right'){
 				$(elem).removeClass('tooltip-left');
 				var tooltip2 = ('<div class="tooltips tooltip-right">' + text + '</div>');
 				$(elem).parent().append(tooltip2);
 				$('#fourth_inp').next('.tooltip-right').css({'left':'104%', 'top':'12%'});
 			};
+	};	
 	
-	};
-  
+
+
   return {
-      init: init
+      init: init, 
   };
   
 })();
 
-validationFn2.init();
+
+if($('.form')) {
+	validationFn.init();
+};
